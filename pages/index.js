4c83f10510258d2ecbf0a46561b0dca60cc75ffb
Harit Panchal <panchalharit007@gmail.com>
2022-09-08 19:25:29 +0530

    meetups added for testing

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,22 @@ const DUMMY_MEETUPS = [
     address: "56 Some Address, Old City",
     description: "This is the second meetup",
   },
+  {
+    id: "m3",
+    title: "Third Meetup",
+    image:
+      "https://images.unsplash.com/photo-1661039709699-9202ea61a2c7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    address: "15 Some Address, New City",
+    description: "This is the first meetup",
+  },
+  {
+    id: "m4",
+    title: "Fourth Meetup",
+    image:
+      "https://images.unsplash.com/photo-1661074578833-59d16206fc7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1475&q=80",
+    address: "56 Some Address, Old City",
+    description: "This is the second meetup",
+  },
 ];
 const index = (props) => {
   const [loadedMeetups, setLoadedMeetups] = useState([]);
